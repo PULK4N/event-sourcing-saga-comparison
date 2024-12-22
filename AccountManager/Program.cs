5660f26997ce1581521dbb6aa2e055a18591f860
Nikola Pupovac <47610156+PULK4N@users.noreply.github.com>
2024-12-22 18:37:02 +0100

    working sending messages

--- AccountManager/Program.cs
+++ AccountManager/Program.cs
@@ -1,3 +1,4 @@
+using AccountManager;
 using AccountManager.BackgroundServices;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHostedService<KafkaMessageReceiverBackgroundService>();
+builder.Services.RegisterKafka(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ AccountManager/RegisterInjection.cs
@@ -0,0 +1,31 @@
+using CommunicationModule;
+using CommunicationModule.Config;
+using CommunicationModule.Interfaces;
+
+namespace AccountManager
+{
+    public static class RegisterInjection
+    {
+        public static IServiceCollection RegisterKafka(
+            this IServiceCollection services,
+            IConfiguration configuration
+        )
+        {
+            services.Configure<KafkaConsumerConfig>(
+                configuration.GetSection("KafkaConsumerConfig")
+            );
+            services.Configure<KafkaProducerConfig>(
+                configuration.GetSection("KafkaProducerConfig")
+            );
+            services.AddSingleton<
+                IMessageProducer<string, string>,
+                KafkaProducer<string, string>
+            >();
+            services.AddSingleton<
+                IMessageConsumer<string, string>,
+                KafkaConsumer<string, string>
+            >();
+            return services;
+        }
+    }
+}
