7b0ecc297afc0aeff9f54cc67a529dd88b733eb4
Nikola Pupovac <nikola.pupovac@rationaletech.com>
2025-05-28 19:19:34 +0200

    added unique constraint to events

--- Event-Sourcing/EventSourcingCoreModules/Persistence/EventSourcingDbContext.cs
+++ Event-Sourcing/EventSourcingCoreModules/Persistence/EventSourcingDbContext.cs
@@ -10,4 +10,13 @@ public class EventSourcingDbContext : DbContext
 
     public DbSet<SerializedEventPayload> SerializedEventPayload { get; set; }
     public DbSet<SerializedPayloadMessage> SerializedPayloadMessage { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder
+            .Entity<SerializedEventPayload>()
+            .HasIndex(ep => new { ep.AggregateId, ep.OrderNumber })
+            .IsUnique();
+    }
 }
---
+++ Event-Sourcing/EventSourcingCoreModules/Persistence/Migrations/20250528171912_UniqueConstraint.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class UniqueConstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SerializedEventPayload_AggregateId_OrderNumber",
+                table: "SerializedEventPayload",
+                columns: new[] { "AggregateId", "OrderNumber" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SerializedEventPayload_AggregateId_OrderNumber",
+                table: "SerializedEventPayload");
+        }
+    }
+}
